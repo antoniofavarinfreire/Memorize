75e80c46c06508d63233b89e5246d0d4b7d2ee3f
Antonio Favarin Freire <42979335+antoniofavarinfreire@users.noreply.github.com>
2022-10-07 09:01:04 -0300

    Initial Commit

---
+++ Memorise2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorise2
+//
+//  Created by Objetiva.digital on 07/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorise2/Memorise2App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorise2App.swift
+//  Memorise2
+//
+//  Created by Objetiva.digital on 07/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Memorise2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
