56322bba50e9320d1da97bc8654d24e3d9a893ac
Antonio Favarin Freire <42979335+antoniofavarinfreire@users.noreply.github.com>
2022-10-10 08:58:58 -0300

    Commit inicial

--- Memorise2/ContentView.swift
+++ Memorise2/ContentView.swift
@@ -1,21 +1,60 @@
 //
-//  ContentView.swift
-//  Memorise2
+// definição de variavel / variavel /tipo da variavel
+//              var         i       :   int
+//              var         b       :   String
 //
-//  Created by Objetiva.digital on 07/10/22.
-//
+// Definição em blocos
+//  Em orientação a objetos em switft, para instanciarmos uma classe, adicionamos '.' nome
+// da classe
 
+// Argumentos em swift são labels
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        
+        .padding(.horizontal)
+        .foregroundColor(.red)
+        
     }
 }
+
+struct CardView: View {
+    
+    @State var isFaceUp : Bool = true //Se torna um ponteiro de boolean
+    
+    var body: some View{
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp{
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("✈️").font(.largeTitle)
+            }else {
+                shape.fill()
+            }
+            
+            
+        }
+        .onTapGesture{
+            isFaceUp = !isFaceUp
+            
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
